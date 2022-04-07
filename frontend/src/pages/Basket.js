17cfdcb9331a05b02f5e53e12325b13ccd3eecf3
Daniel Ugarov <tyradire@yandex.ru>
2022-04-07 19:24:28 +0300

    add components

---
+++ frontend/src/pages/Basket.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Basket extends Component {
+  render() {
+    return (
+      <div>
+        Basket
+      </div>
+    );
+  }
+}
+
+export default Basket;
