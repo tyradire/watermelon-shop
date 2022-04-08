bd479a0abbe9889df095b7fe05fa341e008a3e04
Daniel Ugarov <tyradire@yandex.ru>
2022-04-08 22:07:01 +0300

    add mobx

--- frontend/src/components/AppRouter.js
+++ frontend/src/components/AppRouter.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { createContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes';
+import { SHOP_ROUTE } from '../utils/consts';
+import { Context } from '../index';
 
 function AppRouter(props) {
+
+  const {user} = createContext(Context);
+  console.log(user);
+
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      {user.isAuth && authRoutes.map(({path, Component}) => 
+        <Route key={path} path={path} component={Component} exact/>
+      )}
+      {publicRoutes.map(({path, Component}) => 
+        <Route key={path} path={path} component={Component} exact/>
+      )}
+      <Navigate to={SHOP_ROUTE} />
+    </Routes>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserStore from './store/UserStore';
+
+export const Context = createContext(null);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Context.Provider value={{
+      user: new UserStore()
+    }}>
+      <App />
+    </Context.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
