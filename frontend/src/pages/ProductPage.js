d78d55651024b38f1e23410a7a8aaea76419a6da
Daniel Ugarov <tyradire@yandex.ru>
2022-04-10 23:09:33 +0300

    add ProductPage.js component

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+import like from '../assets/like.svg';
 
-function ProductPage(props) {
+const ProductPage = () => {
+
+  const product = {id: 1, name: 'Квадратный арбуз', price: 500, rating: 5, info: 'Однолетнее травянистое растение, вид рода Арбуз (Citrullus) семейства Тыквенные (Cucurbitaceae)', img: "https://i0.wp.com/sadovodu.com/wp-content/uploads/2017/10/kvadratnyy_arbuz_12_26083819.png"}
+
   return (
-    <div>
-      
-    </div>
+    <Container className='mt-5 d-flex'>
+      <Col md={4}>
+        <Image width={300} height={300} src={product.img}/>
+      </Col>
+      <Col md={4} className='mt-4'>
+        <Row className='d-flex flex-column align-items-center'>
+          <h2>{product.name}</h2>
+        </Row>
+        <div>
+          {product.info}
+        </div>
+        <div 
+          className='d-flex align-items-center justify-content-center mt-2'
+          style={{background: `url(${like}) no-repeat center center`, width: 40, height: 40, backgroundSize: 'contain'}}
+        >
+          {product.rating}
+        </div>
+      </Col>
+      <Col md={4}>
+        {product.price}
+      </Col>
+    </Container>
   );
-}
+};
 
 export default ProductPage;
