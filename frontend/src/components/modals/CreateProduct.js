293f1e95608068b316f944f2754033e667261870
Daniel Ugarov <tyradire@yandex.ru>
2022-04-11 11:38:05 +0300

    edit CreateProduct.js component

--- frontend/src/components/modals/CreateProduct.js
+++ frontend/src/components/modals/CreateProduct.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Button, Col, Dropdown, Form, Modal, Row } from 'react-bootstrap';
+import { Context } from '../../index';
 
 const CreateProduct = ({ show, onHide }) => {
+
+  const { product } = useContext(Context);
+  const [info, setInfo] = useState([]);
+
+  const addInfo = () => {
+    setInfo([...info, {title: '', description: '', number: Date.now()}])
+  }
+
   return (
     <Modal
       show={show}
@@ -16,10 +25,61 @@ const CreateProduct = ({ show, onHide }) => {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        
+        <Form>
+          <Dropdown className='mt-2 mb-2'>
+            <Dropdown.Toggle>Выберите производителя</Dropdown.Toggle>
+            <Dropdown.Menu>
+              {product.vendors.map(vendor =>
+                <Dropdown.Item key={vendor.id}>{vendor.name}</Dropdown.Item>  
+              )}
+            </Dropdown.Menu>
+            <Form.Control 
+              className='mt-3'
+              placeholder='Введите название продукта'
+            />
+            <Form.Control 
+              className='mt-3'
+              placeholder='Введите стоимость продукта'
+              type='number'
+            />
+            <Form.Control 
+              className='mt-3'
+              type='file'
+            />
+            <hr/>
+            <Button
+              variant='outline-dark'
+              onClick={addInfo}
+            >
+              Добавить новое свойство
+            </Button>
+            {info.map(i =>
+              <Row>
+                <Col md={4}>
+                  <Form.Control 
+                    placeholder='Введите название свойства'
+                  />
+                </Col>
+                <Col md={4}>
+                  <Form.Control 
+                    placeholder='Введите описание свойства'
+                  />
+                </Col>
+                <Col md={4}>
+                  <Button
+                    variant='outline-danger'
+                  >
+                    Удалить
+                  </Button>
+                </Col>
+              </Row>
+            )}
+          </Dropdown>
+        </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={onHide}>Close</Button>
+        <Button variant='outline-danger' onClick={onHide}>Закрыть</Button>
+        <Button variant='outline-success' onClick={onHide}>Добавить</Button>
       </Modal.Footer>
     </Modal>
   );
--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
+import CreateProduct from '../components/modals/CreateProduct';
+import CreateVendor from '../components/modals/CreateVendor';
 
 const Admin = () => {
+
+  const [vendorVisible, setVendorVisible] = useState(false);
+  const [productVisible, setProductVisible] = useState(false);
+
   return (
     <Container className='d-flex flex-column'>
-      <Button variant='outline-dark' className='mt-4 p2'>Добавить производителя</Button>
-      <Button variant='outline-dark' className='mt-4 p2'>Добавить продукт</Button>
+      <Button variant='outline-dark' className='mt-4 p2' onClick={() => setVendorVisible(true)} >Добавить производителя</Button>
+      <Button variant='outline-dark' className='mt-4 p2' onClick={() => setProductVisible(true)} >Добавить продукт</Button>
+      <CreateProduct show={productVisible} onHide={() => setVendorVisible(false)} />
+      <CreateVendor show={vendorVisible} onHide={() => setProductVisible(false)}/>
     </Container>
   );
 };
