a09f1b251e7437ae826369d00097e0da2ecf9792
Daniel Ugarov <tyradire@yandex.ru>
2022-04-12 22:10:06 +0300

    add getUser func

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useContext, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Context } from './index';
 import AppRouter from './components/AppRouter';
 import NavBar from './components/NavBar';
+import { Spinner } from 'react-bootstrap';
+import { get }
 
-const App = () => {
+const App = observer(() => {
+
+  const {user} = useContext(Context);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    
+  })
+
+  if (loading) {
+    return <Spinner className='d-flex align-items-center justify-content-center' animation={"grow"}/>
+}
+
   return (
     <BrowserRouter>
       <NavBar />
       <AppRouter />
     </BrowserRouter>
   );
-};
+});
 
 export default App;
