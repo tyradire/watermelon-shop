3437aee78b47c62cef7eaefb1aa0616371ae606f
Daniel Ugarov <tyradire@yandex.ru>
2022-04-18 03:06:10 +0300

    minor edit BasketItem.js component

--- frontend/src/components/BasketItem.js
+++ frontend/src/components/BasketItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import seer from '../assets/seer.JPG';
 import './BasketItem.css';
 
 const BasketItem = (props) => {
 
-  console.log('props ', props.product)
+  // console.log('props ', props.product)
 
   return (
     <div className='product'>
-      <img alt={'product'} src={seer}/>
+      <img className='product__image' alt={'product'} src={process.env.REACT_APP_PUBLIC_URL + props.product['img']}/>
       <p className='product__title'>{ props.product['name'] }</p>
       <p className='product__count'>1 шт</p>
       <p className='product__price'>{ props.product['price'] } &#8381;</p> 
