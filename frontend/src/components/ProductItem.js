f9a42ee6d9c77a72b9ce05afe9c013b85d8e425a
Daniel Ugarov <tyradire@yandex.ru>
2022-04-18 03:45:27 +0300

    add button ProductItem.js

--- frontend/src/components/ProductItem.js
+++ frontend/src/components/ProductItem.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Card, Col, Image } from 'react-bootstrap';
+import { Button, Card, Col, Image } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import like from '../assets/like.svg';
 import { PRODUCT_ROUTE } from '../utils/consts';
+import { addToBasket } from '../utils/BasketApi';
 import './ProductItem.css';
 
 const ProductItem = ({ product, vendor }) => {
 
   const navigate = useNavigate();
 
+  const addProduct = () => {
+    addToBasket(product.id)
+  };
+
   return (
-    <Col md={3} className='mt-1' onClick={() => navigate(PRODUCT_ROUTE + '/' + product.id)}>
-      <Card style={{width: 180, cursor: 'pointer', borderRadius: 4}} border={'light'} bg={'light'} >
+    <Col md={3} className='mt-1' >
+      <Card onClick={() => navigate(PRODUCT_ROUTE + '/' + product.id)} style={{width: 180, cursor: 'pointer', borderRadius: 4}} border={'light'} bg={'light'} >
         <Image className='product-item__image' width={178} height={178} src={process.env.REACT_APP_PUBLIC_URL + product.img}/>
         <div className='text-black-50 mt-1 mx-2 d-flex justify-content-between align-items-center'>
           <div className='product-item__product-name'>{product.name}</div>
@@ -22,7 +27,9 @@ const ProductItem = ({ product, vendor }) => {
         </div>
         <div style={{textAlign: 'center'}} className='text-black-50 product-item__vendor-name'>{vendor}</div>
       </Card>
+      <Button style={{width: 180}} className="rounded-0" variant="outline-success" size="sm" onClick={addProduct}>Добавить в корзину</Button>
     </Col>
+    
   );
 };
 
