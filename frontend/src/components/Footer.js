f255bb587dd77b8a42dcbc3a7d22b4822ac1b095
Daniel Ugarov <tyradire@yandex.ru>
2022-06-13 20:59:21 +0300

    edit product card

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import githubIcon from '../assets/github-icon.svg';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <div className='footer'>
-      <Link className='footer__link' to={'https://github.com/tyradire'} target="_blank">
-        <img className='footer__link-icon' href={githubIcon} />
-      </Link>
+      <a className='footer__link' href='https://github.com/tyradire' target="_blank"></a>
     </div>
   );
 };
